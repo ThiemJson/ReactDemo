06b0c1e4046d99c546963859f3ba6d02463dcfe3
nguyen_thiem <nguyencaothiem292@gmail.com>
2022-08-02 09:47:01 +0700

    Fetch photos

--- src/components/photos/Photo.js
+++ src/components/photos/Photo.js
@@ -2,39 +2,40 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 //https://picsum.photos/v2/list
 
-export const fetchPhotos = (url) => {
-  return axios
-    .get(url)
-    .then(({ data }) => {
-      return data;
-    })
-    .catch((error) => {
-      return error;
-    });
+export const fetchPhotos = async (url) => {
+  try {
+    console.log("Pre fetch");
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const Photo = () => {
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    const result = fetchPhotos(
+  const handlerFetchPhotos = async () => {
+    const images = await fetchPhotos(
       `https://picsum.photos/v2/list?page=${page}&limit=8`
     );
-    result.then((responsePhotos) => {
-      const newPhotos = [...photos, ...responsePhotos];
-      console.log(newPhotos);
-      setPhotos(newPhotos);
-    });
-  }, [page]);
+    const newPhotos = [...photos, ...images];
+    setPhotos(newPhotos);
+    setPage((page) => page + 1);
+  };
+
+  useEffect(() => {
+    handlerFetchPhotos();
+  }, []);
 
   return (
     <div>
-      <div className=" grid grid-cols-4 gap-[20px] bg-red-500">
+      <div className=" m-3 grid grid-cols-4 gap-[20px]">
         {photos.map(({ id, download_url, author }, index) => (
           <div
             key={index}
-            className="bg-white rounded-lg p-[10px] h-[200px] flex flex-col"
+            className="bg-white rounded-lg p-[10px] h-[200px] flex flex-col shadow-md"
           >
             <div className="flex justify-between item-center mb-[10px]">
               <span>{id}</span>
@@ -52,7 +53,7 @@ export const Photo = () => {
         <button
           className=" inline-block px-8 py-4 bg-purple-600 text-[#fff] rounded-lg"
           onClick={() => {
-            setPage((page) => page + 1);
+            handlerFetchPhotos();
           }}
         >
           Load more
